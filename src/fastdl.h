7ac5512e81bea430de8d71b7a83e3fab66e16bc0
Byeonggon Lee <gonny952@gmail.com>
2018-12-15 17:07:47 +0000

    Added FASTDL_SYM, Using this macro removes uninitialized value warnings from valgrind

--- src/fastdl.h
+++ src/fastdl.h
@@ -16,6 +16,26 @@ struct fastdl_Handle {
 
 int fastdl_open(struct fastdl_Handle* handle, const char* path, int flag);
 int fastdl_sym(struct fastdl_Handle* handle, const char* symbolName, void** symbolPointer);
+
+#define FASTDL_SYM(handle, symbolName, symbolPointer, ret) \
+do { \
+    struct fastdl_Symbol* symbol = NULL; \
+    size_t symbolNameLength = strlen(symbolName); \
+    HASH_FIND(hashHandle, (handle)->cache, &(symbolName), symbolNameLength, symbol); \
+    if(symbol != NULL) { \
+        *(symbolPointer) = symbol->pointer; \
+        *(ret) = 1; \
+    } \
+    *(symbolPointer) = dlsym((handle)->dlHandle, (symbolName)); \
+    if(*(symbolPointer) == NULL) \
+        *(ret) = -1; \
+    symbol = calloc(1, sizeof(struct fastdl_Symbol)); \
+    symbol->name = (symbolName); \
+    symbol->pointer = *(symbolPointer); \
+    HASH_ADD(hashHandle, (handle)->cache, name, symbolNameLength, symbol); \
+    *(ret) = 0; \
+} while(0);
+
 int fastdl_close(struct fastdl_Handle* handle);
 char* fastdl_error();
 
